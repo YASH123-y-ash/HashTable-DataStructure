8c8b10155559e9aa463fd62f8f443c9342a65d16
Yash-y-ash <wardhan696@gmail.com>
2021-04-27 01:15:40 +0530

    Added:ability to find word frequency in a paragraph

--- test/java/MyHashMapTest.java
+++ test/java/MyHashMapTest.java
@@ -1,4 +1,5 @@
 import com.hashmap.MyHashMap;
+import com.hashmap.MyLinkedHashMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,23 @@ public class MyHashMapTest {
         int frequency = myHashMap.get("to");
         System.out.println(myHashMap);
         Assertions.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void givenParagraph_WhenWordsAreAdded_ShouldReturnFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into " +
+                "paranoid avoidable situations";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap);
+        int frequency = myLinkedHashMap.get("paranoid");
+        Assertions.assertEquals(3, frequency);
     }
 }
